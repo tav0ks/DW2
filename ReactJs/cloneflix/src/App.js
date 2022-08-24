f04d603f195c75ad010c33f2a2770c32a8da196d
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-08-23 21:11:56 -0300

    Realizadas importações do Tmdb

--- ReactJs/cloneflix/src/App.js
+++ ReactJs/cloneflix/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tmdb from './Tmdb.js';
 
-function App() {
+export default () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
